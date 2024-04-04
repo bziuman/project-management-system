43a0b4c017c08eccd116d00e8ed5e88c9279c5ad
bziuman <bohdanziuman@gmail.com>
2024-04-04 21:34:09 +0300

    added auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.getTokenFromHeader(request);
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request['user'] = payload.user;
+    } catch (err) {
+      throw new UnauthorizedException('');
+    }
+    return true;
+  }
+
+  private getTokenFromHeader(req: Request): string | undefined {
+    const [type, token] = req.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
